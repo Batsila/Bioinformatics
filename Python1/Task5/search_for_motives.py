52f5806070c6f4e91f5508077e9598309465c847
Arsienij Aniščanka <BatsilaBox3@gmail.com>
2019-02-21 18:11:30 +0300

    Task 5b complited

--- Python1/Task5/search_for_motives.py
+++ Python1/Task5/search_for_motives.py
@@ -1,8 +1,18 @@
 import re
 
 input = open("dna.txt", "r")
-dna = input.read()
-motives = re.finditer(r'GC\wGC', dna)
+output = open("result.txt", "w+")
 
-for motive in motives:
-    print (str(motive.start()))
+dna = input.read()
+motives_a = re.finditer(r'GC\wGC', dna)
+
+output.write("Motive a in ")
+for motive in motives_a:
+    output.write(str(motive.start()) + " ")
+
+motives_b = re.finditer(r'ATG[ATGC]{3,1000}[A]{5,10}', dna)
+
+output.write("\nMotive b in ")
+for motive in motives_b:
+    output.write(str(motive.start()) + " ")
+
